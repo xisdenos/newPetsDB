96c2d90d0d0a0927954ce3836d7fdabc0fcdbf3b
razante <lucaspinto.andrade@gmail.com>
2022-01-11 00:49:58 -0300

    refactor: contextStrategy moved to its own document

---
+++ src/db/strategies/base/contextStrategy.js
@@ -0,0 +1,21 @@
+class ContextStrategy extends ICrud {
+    constructor(strategy) {
+        this._database = strategy
+    }
+
+    create(item) {
+        return this._database.create(item)
+    }
+
+    read(item) {
+        return this._database.read(item)
+    }
+
+    update(id, item) {
+        return this._database.update(id, item)
+    }
+
+    delete(item) {
+        return this._database.delete(id)
+    }
+}
