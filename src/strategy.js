ee48302df6821e7a074ebc35436256c8e4413cb3
razante <lucaspinto.andrade@gmail.com>
2022-01-11 00:17:26 -0300

    chore: error interface, crud and context interface added

--- src/strategy.js
+++ src/strategy.js
@@ -18,8 +18,30 @@ class ICrud {
     update(id, item) {
         throw new NotImplementedException()
     }
-    
+
     delete(id) {
         throw new NotImplementedException()
+    }
+}
+
+class ContextStrategy extends ICrud {
+    constructor(strategy) {
+        this._database = strategy
+    }
+
+    create(item) {
+        return this._database.create(item)
+    }
+
+    read(item) {
+        return this._database.read(item)
+    }
+
+    update(id, item) {
+        return this._database.update(id, item)
+    }
+
+    delete(item) {
+        return this._database.delete(id)
     }
 }
